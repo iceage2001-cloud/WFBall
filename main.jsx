0af8b2f2818809ce8f4390186192f96245cb8d3c
iceage2001-cloud <iceage2001@gmail.com>
2026-06-14 10:44:06 +0530

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import WCQuizApp from "./WCQuizApp.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <WCQuizApp />
+  </React.StrictMode>
+);
